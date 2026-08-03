6be743cb18f528ee72972026c9b5fa80966c8a5b
pasqlanotte <lanottepasquale@gmail.com>
2026-08-03 09:26:59 +0200

    Delete AD8232/installa.py

--- AD8232/installa.py
+++
@@ -1,7 +0,0 @@
-import os
-
-with open("requirements.txt", "r") as file:
-    for pacchetto in file.read().splitlines():
-        if pacchetto:  # Controlla che la riga non sia vuota
-            print(f"Installando {pacchetto}...")
-            os.system(f"mpremote mip install --target lib {pacchetto}")
